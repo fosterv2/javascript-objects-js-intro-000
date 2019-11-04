9b823a37b161fac1c5e759e0f5d109b17aa8ed92
Valerie Foster <fosterv2@outlook.com>
2019-11-04 00:59:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({'Sara Bareilles' : 'Gravity'})
 
 function updatePlaylist(play, name, title) {
-  return Object.assign({}, play, { [name] : title })
+  play[name] = title
 }
 
 function removeFromPlaylist(play, name) {
