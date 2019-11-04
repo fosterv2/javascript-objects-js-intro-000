58904928359e416c0e8193c5d3a123e4fd31e15f
Valerie Foster <fosterv2@outlook.com>
2019-11-04 00:47:55 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {};
+
+function updatePlaylist(playlist, name, title) {
+  return Object.assign({}, playlist, { [name] : title })
+}
+
+function removeFromPlaylist(playlist, name) {
+  return delete playlist.name
+}
